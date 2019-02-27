b6c47dd81d94dfd016bc95547735c256b8d2b2af
Stephen French <stephenfrench9@gmail.com>
2019-02-27 11:34:45 -0800

    stretched my forearms, typing was easier. As far as the code goes, chose a different invarient for the while loop that traverses the input linkied list. I also wrote tests for small lists, med lists, and large lists, as well as I am now working on testing for repeated elements in each of those situations.

--- remove-linked-list-elements/src/Tests.java
+++ remove-linked-list-elements/src/Tests.java
@@ -5,12 +5,11 @@ import src.Solution;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import static junit.framework.TestCase.fail;
 
 public class Tests {
-    ListNode example;
-
     public ListNode dummy_list() {
         ListNode head = new ListNode(0);
         ListNode curr = head;
@@ -93,15 +92,209 @@ public class Tests {
     }
 
     @Test(timeout=1000)
-    public void first(){
+    public void small_lists() {
+        //empty initial list
+        ListNode initial = null;
+        ListNode head = Solution.removeElements(initial, 0);
+        ListNode correct_answer = null;
+        lists_match(head, correct_answer);
 
-        ListNode l = dummy_list();
-        print_ll(l);
-        System.out.println("****************");
+        //size=1, remove it
+        initial = linked_list(
+                new Integer[] {0});
+        head = Solution.removeElements(initial, 0);
+        correct_answer = null;
+        lists_match(head, correct_answer);
 
-        ListNode head = Solution.removeElements(l, 1);
+        //size=1, leave it
+        initial = linked_list(
+                new Integer[] {0});
+        head = Solution.removeElements(initial, 5);
+        correct_answer = linked_list(
+                new Integer[] {0});
+        lists_match(head, correct_answer);
 
-        print_ll(head);
+        //size=2, remove nothing
+        initial = linked_list(
+                new Integer[] {0, 2});
+        head = Solution.removeElements(initial, 5);
+        correct_answer = linked_list(
+                new Integer[] {0, 2});
+        lists_match(head, correct_answer);
+
+        //size=2, remove leading element
+        initial = linked_list(
+                new Integer[] {0, 2});
+        head = Solution.removeElements(initial, 0);
+        correct_answer = linked_list(
+                new Integer[] {2});
+        lists_match(head, correct_answer);
+
+        //size=2, remove trailing element
+        initial = linked_list(
+                new Integer[] {0, 2});
+        head = Solution.removeElements(initial, 2);
+        correct_answer = linked_list(
+                new Integer[] {0});
+        lists_match(head, correct_answer);
+    }
+
+    @Test(timeout=1000)
+    public void reg_lists() {
+
+        Integer[] raw = new Integer[] {0, 1, 2, 3, 4, 5, 6};
+        Integer target = 0;
+        Integer[] result = new Integer[] {1, 2, 3, 4, 5, 6};
+        verify_removal(raw, target, result);
+
+        ListNode initial = linked_list(
+                new Integer[] {0, 1, 2, 3, 4, 5, 6});
+        ListNode head = Solution.removeElements(initial, 6);
+        ListNode correct_answer = linked_list(
+                new Integer[] {0, 1, 2, 3, 4, 5});
+        lists_match(head, correct_answer);
+
+        initial = linked_list(
+                new Integer[] {0, 1, 2, 3, 4, 5, 6});
+        head = Solution.removeElements(initial, 3);
+        correct_answer = linked_list(
+                new Integer[] {0, 1, 2, 4, 5, 6});
+        lists_match(head, correct_answer);
+
+        initial = linked_list(
+                new Integer[] {0, 1, 2, 3, 4, 5, 6});
+        head = Solution.removeElements(initial, 1);
+        correct_answer = linked_list(
+                new Integer[] {0, 2, 3, 4, 5, 6});
+        lists_match(head, correct_answer);
+
+        initial = linked_list(
+                new Integer[] {0, 1, 2, 3, 4, 5, 6});
+        head = Solution.removeElements(initial, 5);
+        correct_answer = linked_list(
+                new Integer[] {0, 1, 2, 3, 4, 6});
+        lists_match(head, correct_answer);
+    }
+
+    private void verify_removal(Integer[] raw, Integer target,Integer[] desired_result) {
+        ListNode raw_ll = linked_list(raw);
+        ListNode result = Solution.removeElements(raw_ll, target);
+        ListNode desired_result_ll = linked_list(desired_result);
+        lists_match(result, desired_result_ll);
+    }
+
+    @Test(timeout=1000)
+    public void large_lists() {
+
+        //remove middle
+        Integer[] a = new Integer[10000];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        ListNode initial = linked_list(a);
+        ListNode head = Solution.removeElements(initial, 5000);
+        Integer[] b = new Integer[9999];
+        for(int i = 0; i < 5000; i++) {
+            b[i] = i;
+        }
+        for(int i = 5000; i < 9999; i++) {
+            b[i] = i+1;
+        }
+        ListNode correct_answer = linked_list(b);
+        lists_match(head, correct_answer);
+
+        //remove front
+        a = new Integer[10000];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        initial = linked_list(a);
+        head = Solution.removeElements(initial, 0);
+        b = new Integer[9999];
+        for(int i = 0; i < 9999; i++) {
+            b[i] = i+1;
+        }
+        correct_answer = linked_list(b);
+        lists_match(head, correct_answer);
+
+        //remove end
+        a = new Integer[10000];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        initial = linked_list(a);
+        head = Solution.removeElements(initial, 9999);
+        b = new Integer[9999];
+        for(int i = 0; i < b.length; i++) {
+            b[i] = i;
+        }
+        correct_answer = linked_list(b);
+        lists_match(head, correct_answer);
+    }
+
+    @Test(timeout=1000)
+    public void repeated_elements_small() {
+        //list entirely composed of repeated elements (this test caught an issue)
+        ListNode initial = linked_list(
+                new Integer[] {0, 0});
+        ListNode head = Solution.removeElements(initial, 0);
+        ListNode correct_answer = null;
+        lists_match(head, correct_answer);
+
+        //repeated non-target elements
+        initial = linked_list(
+                new Integer[] {0, 0});
+        head = Solution.removeElements(initial, 5);
+        correct_answer = linked_list(
+                new Integer[] {0, 0});
+        lists_match(head, correct_answer);
+
+        //repeated end elements
+        initial = linked_list(
+                new Integer[] {0, 2, 2});
+        head = Solution.removeElements(initial, 2);
+        correct_answer = linked_list(
+                new Integer[] {0});
+        lists_match(head, correct_answer);
+
+        //repeated leading elements
+        initial = linked_list(
+                new Integer[] {2, 2, 0});
+        head = Solution.removeElements(initial, 2);
+        correct_answer = linked_list(
+                new Integer[] {0});
+        lists_match(head, correct_answer);
+
+        //repeated elements in the middle
+        initial = linked_list(
+                new Integer[] {0, 2, 2, 0});
+        head = Solution.removeElements(initial, 2);
+        correct_answer = linked_list(
+                new Integer[] {0, 0});
+        lists_match(head, correct_answer);
+
+        //non contiguous repeated elements
+        initial = linked_list(
+                new Integer[] {2, 0, 0, 0});
+        head = Solution.removeElements(initial, 2);
+        correct_answer = linked_list(
+                new Integer[] {0, 0, 0});
+        lists_match(head, correct_answer);
+    }
+
+    @Test(timeout=1000)
+    public void repeated_elements_reg() {
 
     }
+
+    @Test(timeout=1000)
+    public void repeated_elements_large() {
+
+    }
+
 }
+
+
+
+
+
--- remove-linked-list-elements/src/src/Solution.java
+++ remove-linked-list-elements/src/src/Solution.java
@@ -11,6 +11,11 @@ public class Solution {
     }
 
     public static ListNode removeElements(ListNode head, int val) {
+        //Make sure first element in the list is valid
+        while(head != null && head.val == val) {
+            head = head.next;
+        }
+
         // 0
         if(head == null) {
             return null;
@@ -18,32 +23,53 @@ public class Solution {
 
         //1
         if(head.next == null) {
-            return head;
+            if(head.val == val) {
+                return null;
+            } else {
+                return head;
+            }
+        }
+
+        //body. invarient: prev is legit, current we might need to get rid of.
+        //invarient is easy to initialize
+        ListNode prev = head;
+        ListNode curr = head.next;
+        while(curr != null) {
+            curr = eval_curr(val, prev, curr);
+            //prev is a legit node, curr is either legit or null
+            if(curr != null) {
+                prev = curr;
+                curr = curr.next;
+            }
         }
 
         //initialize invarient - the hard part.
-        ListNode prev = head;
-        ListNode curr = head.next;
-        eval_curr(val, prev, curr);
+//        ListNode prev = head;
+//        ListNode curr = head.next;
+//        eval_curr(val, prev, curr);
 
         //body. prev and curr point at legit nodes.
-        while(curr.next != null) {
-            prev = curr;
-            curr = curr.next;
-            eval_curr(val, prev, curr);
-        }
+//        while(curr.next != null) {
+//            prev = curr;
+//            curr = curr.next;
+//            eval_curr(val, prev, curr);
+//        }
 
         return head;
     }
 
-    private static void eval_curr(int val, ListNode prev, ListNode curr) {
+    // updates curr until it points to a valid node or it is null.
+    // returns: the new curr.
+    private static ListNode eval_curr(int val, ListNode prev, ListNode curr) {
         while(curr != null && curr.val == val) {
             if(curr.next == null) {
                 prev.next = null;
+                curr = null;
             } else {
                 prev.next = curr.next;
                 curr = curr.next;
             }
         }
+        return curr;
     }
 }
