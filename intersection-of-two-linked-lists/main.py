677377b9ac77190dbb3611c0a3fdc7b12be0b4b5
Stephen French <stephenfrench9@gmail.com>
2019-06-03 14:08:12 -0700

    intersection-of-two-linked-lists. most upvoted solution from the discussion. In principle uses very little memory and runs in linear time, but doesn't use much less sapace than when I uesed the default dict

--- intersection-of-two-linked-lists/main.py
+++ intersection-of-two-linked-lists/main.py
@@ -19,50 +19,73 @@ class ListNode(object):
 
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
-        df = defaultdict()
 
-        cur = headA
-        while cur:
-            df[cur] = 1
-            cur = cur.next
+        cura = headA
+        curb = headB
 
-        cur = headB
-        while cur:
-            if df.get(cur):
-                return cur
-            cur = cur.next
+        while cura != curb:
+            if cura is not None:
+                cura = cura.next
+            else:
+                cura = headB
 
-        return None
+            if curb is not None:
+                curb = curb.next
+            else:
+                curb = headA
+
+
+        return cura
+
 
 if __name__ == '__main__':
-    # a = ListNode(1)
-    # a.next = ListNode(2)
-    # a.next.next = ListNode(3)
-    # a.next.next.next = ListNode(3)
-    # a.next.next.next.next = ListNode(2)
-    # a.next.next.next.next.next = ListNode(1)
-    # b = ListNode(1)
-    # b.next = ListNode(2)
-    # b.next.next = ListNode(3)
-    # b.next.next.next = ListNode(3)
-    # b.next.next.next.next = ListNode(2)
-    # b.next.next.next.next.next = ListNode(1)
-    # printl(a, "argument: ")
-    # actual = Solution.getIntersectionNode(Solution, a, b)
-    # printl(a, "argument: ")
-    # if actual:
-    #     print(actual.val)
-    # else:
-    #     print(actual)
-    # print()
-
-
+    a = ListNode(1)
+    a.next = ListNode(2)
+    a.next.next = ListNode(3)
+    a.next.next.next = ListNode(3)
+    a.next.next.next.next = ListNode(2)
+    a.next.next.next.next.next = ListNode(1)
+    b = ListNode(1)
+    b.next = ListNode(2)
+    b.next.next = ListNode(3)
+    b.next.next.next = ListNode(3)
+    b.next.next.next.next = ListNode(2)
+    b.next.next.next.next.next = ListNode(1)
+    printl(a, "argument: ")
+    actual = Solution.getIntersectionNode(Solution, a, b)
+    printl(a, "argument: ")
+    if actual:
+        print(actual.val)
+    else:
+        print(actual)
+    print()
 
     a = ListNode(1)
     a.next = ListNode(2)
     a.next.next = ListNode(3)
     b = ListNode(1)
     b.next = a.next
+    printl(a, "argument a: ")
+    printl(b, "argument b: ")
+    actual = Solution.getIntersectionNode(Solution, a, b)
+    printl(a, "argument a: ")
+    printl(b, "argument b: ")
+    if actual:
+        print("shared value is: %s " % actual.val)
+    else:
+        print(actual)
+    print()
+
+    a = ListNode(1)
+    a.next = ListNode(2)
+    a.next.next = ListNode(3)
+    a.next.next.next = ListNode(3)
+    a.next.next.next.next = ListNode(22)
+    a.next.next.next.next.next = ListNode(1)
+    b = ListNode(10)
+    b.next = ListNode(11)
+    b.next.next = ListNode(12)
+    b.next.next.next = a.next.next.next.next
 
     printl(a, "argument a: ")
     printl(b, "argument b: ")
