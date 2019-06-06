02c0ce47c0bab6dc4c081a8a0c113acf59eddb50
Stephen French <stephenfrench9@gmail.com>
2019-06-05 18:31:29 -0700

    middle-of-the-linked-list. runs in the 5 to 97 percentile

---
+++ middle-of-the-linked-list/main.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+def printl(l, header):
+    cur = l
+    print(header, end=": ")
+    while cur:
+        print(cur.val, end=", ")
+        cur = cur.next
+    print()
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def middleNode(self, head):
+        if not head:
+            return None
+        if not head.next:
+            return head
+
+        n = 0
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            n = n + 1
+
+        return slow
+
+
+
+if __name__ == '__main__':
+    print("CASE")
+    a = ListNode(1)
+    a.next = ListNode(2)
+    a.next.next = ListNode(3.5)
+    a.next.next.next = ListNode(3)
+    a.next.next.next.next = ListNode(2)
+    # a.next.next.next.next.next = ListNode(1)
+    printl(a, "argument: ")
+    actual = Solution.middleNode(Solution, a)
+    printl(a, "argument: ")
+    print(actual.val)
+
+
+
