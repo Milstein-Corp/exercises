ffe2cfd08c9977c6afe1cf30022dbc6877288363
Stephen French <stephenfrench9@gmail.com>
2019-06-22 22:48:04 -0700

    valid-anagram, solved by sorting. Also can be solved with a dictionary

---
+++ valid-anagram/main.py
@@ -0,0 +1,80 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+
+        one = sorted(s)
+        two = sorted(t)
+
+        return one == two
+
+
+if __name__ == '__main__':
+    s = "anagram"
+    t = "nagaram"
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = True
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
+    s = "rat"
+    t = "car"
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = False
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
+    s = "listen"
+    t = "silent"
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = True
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
+    s = "listena"
+    t = "silent"
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = False
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
+    s = "a"
+    t = "a"
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = True
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
+    s = ""
+    t = ""
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = True
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
+    s = "a"
+    t = ""
+    actual = Solution.isAnagram(Solution, s, t)
+    desired = False
+    print("s, t: %s %s " % (s, t))
+    print("desired : " + str(desired))
+    print("actual  : " + str(actual))
+    assert actual == desired
+    print()
+
