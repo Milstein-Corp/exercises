70b8fba72b403171236cd795780178a4b4db2a32
Stephen French <stephenfrench9@gmail.com>
2019-07-11 13:23:57 -0700

    permuation-in-string: This is how I build windows

--- permutation-in-string/main.py
+++ permutation-in-string/main.py
@@ -1,26 +1,18 @@
 def checkInclusion(s1, s2):
+    windowSize = len(s1)
+    for c in range(0, len(s2)-windowSize+1):
+        for i in range(windowSize):
+            print(c + i, end=", ")
+        print()
 
-    s1 = sorted(s1)
-    front = 0
-    interval = len(s1) - 1
-    back = front + interval
 
-    for c in range(len(s2)-interval):
-
-        # print(c)
-        # print(c + interval + 1)
-        b = sorted(s2[c:c + interval + 1])
-        # print(b)
-        # print(s1)
-        # print()
-        if s1 == b:
-            return True
+    print("last index: %s" % str(len(s2)-1))
 
 
     return False
 
 if __name__ == '__main__':
-    substring = "ab"
+    substring = "abc"
     longstring = "eidbaoooo"
     desired = True
     actual = checkInclusion(substring, longstring)
@@ -31,43 +23,44 @@ if __name__ == '__main__':
     print()
 
 
-    substring = "ab"
-    longstring = "eidboaoooo"
-    desired = False
-    actual = checkInclusion(substring, longstring)
-    print()
-    print("sub, long : %s, %s" % (substring, longstring))
-    print("desired: %s" % desired)
-    print("actual : %s" % actual)
-    print()
-
-    substring = "ei"
-    longstring = "eidboaoooo"
-    desired = True
-    actual = checkInclusion(substring, longstring)
-    print()
-    print("sub, long : %s, %s" % (substring, longstring))
-    print("desired: %s" % desired)
-    print("actual : %s" % actual)
-    print()
-
-    substring = "oj"
-    longstring = "eidboaooooj"
-    desired = True
-    actual = checkInclusion(substring, longstring)
-    print()
-    print("sub, long : %s, %s" % (substring, longstring))
-    print("desired: %s" % desired)
-    print("actual : %s" % actual)
-    print()
-
-    substring = "aaa"
-    longstring = "eidboaaaaooooj"
-    desired = True
-    actual = checkInclusion(substring, longstring)
-    print()
-    print("sub, long : %s, %s" % (substring, longstring))
-    print("desired: %s" % desired)
-    print("actual : %s" % actual)
-    print()
-
+    # substring = "ab"
+    # longstring = "eidboaoooo"
+    # desired = False
+    # actual = checkInclusion(substring, longstring)
+    # print()
+    # print("sub, long : %s, %s" % (substring, longstring))
+    # print("desired: %s" % desired)
+    # print("actual : %s" % actual)
+    # print()
+    #
+    # substring = "ei"
+    # longstring = "eidboaoooo"
+    # desired = True
+    # actual = checkInclusion(substring, longstring)
+    # print()
+    # print("sub, long : %s, %s" % (substring, longstring))
+    # print("desired: %s" % desired)
+    # print("actual : %s" % actual)
+    # print()
+    #
+    # substring = "oj"
+    # longstring = "eidboaooooj"
+    # desired = True
+    # actual = checkInclusion(substring, longstring)
+    # print()
+    # print("sub, long : %s, %s" % (substring, longstring))
+    # print("desired: %s" % desired)
+    # print("actual : %s" % actual)
+    # print()
+    #
+    # substring = "aaa"
+    # longstring = "eidboaaaaooooj"
+    # desired = True
+    # actual = checkInclusion(substring, longstring)
+    # print()
+    # print("sub, long : %s, %s" % (substring, longstring))
+    # print("desired: %s" % desired)
+    # print("actual : %s" % actual)
+    # print()
+#
+#
