65301f9d9e52eb52ca448ad6f06b7c21ce5dcf88
Stephen French <stephenfrench9@gmail.com>
2019-07-11 13:52:27 -0700

    permutation-in-string scores above 95 percentile for runtime, 70% memory. I slide a window across the array. I manually checked the first window, and then had the array do the rest. I make small changes to the representation of the contents of the window with each new chrarcter. I had a bug where one rep had 0 entries in its map like a:0 and the other would just not have a in the map, so I had to go delete all the 0 entries as they showed up>

--- permutation-in-string/main.py
+++ permutation-in-string/main.py
@@ -1,18 +1,55 @@
 def checkInclusion(s1, s2):
     windowSize = len(s1)
-    for c in range(0, len(s2)-windowSize+1):
-        for i in range(windowSize):
-            print(c + i, end=", ")
-        print()
+    substring = {}
+    parent = {}
+
+    for c in s1[0:windowSize]:
+        try:
+            substring[c] += 1
+        except:
+            substring[c] = 1
+
+    for c in s2[0:windowSize]:
+        try:
+            parent[c] += 1
+        except:
+            parent[c] = 1
+
+    if parent == substring:
+        return True
+
+    for c in range(1, len(s2)-windowSize+1):
+        if parent[s2[c-1]] == 1:
+            del parent[s2[c-1]]
+        else:
+            parent[s2[c - 1]] -= 1
+
+        try:
+            parent[s2[c+windowSize-1]] += 1
+        except KeyError:
+            parent[s2[c+windowSize-1]] = 1
+
+        # print("parent c: %s" % c)
+        # print(parent)
+        # print(substring)
 
 
-    print("last index: %s" % str(len(s2)-1))
+        if parent == substring:
+            return True
+
+        # for i in range(windowSize):
+        #     print(c + i, end=", ")
+        # print()
+        # print()
+
+
+    # print("last index: %s" % str(len(s2)-1))
 
 
     return False
 
 if __name__ == '__main__':
-    substring = "abc"
+    substring = "ab"
     longstring = "eidbaoooo"
     desired = True
     actual = checkInclusion(substring, longstring)
@@ -23,44 +60,44 @@ if __name__ == '__main__':
     print()
 
 
-    # substring = "ab"
-    # longstring = "eidboaoooo"
-    # desired = False
-    # actual = checkInclusion(substring, longstring)
-    # print()
-    # print("sub, long : %s, %s" % (substring, longstring))
-    # print("desired: %s" % desired)
-    # print("actual : %s" % actual)
-    # print()
-    #
-    # substring = "ei"
-    # longstring = "eidboaoooo"
-    # desired = True
-    # actual = checkInclusion(substring, longstring)
-    # print()
-    # print("sub, long : %s, %s" % (substring, longstring))
-    # print("desired: %s" % desired)
-    # print("actual : %s" % actual)
-    # print()
-    #
-    # substring = "oj"
-    # longstring = "eidboaooooj"
-    # desired = True
-    # actual = checkInclusion(substring, longstring)
-    # print()
-    # print("sub, long : %s, %s" % (substring, longstring))
-    # print("desired: %s" % desired)
-    # print("actual : %s" % actual)
-    # print()
-    #
-    # substring = "aaa"
-    # longstring = "eidboaaaaooooj"
-    # desired = True
-    # actual = checkInclusion(substring, longstring)
-    # print()
-    # print("sub, long : %s, %s" % (substring, longstring))
-    # print("desired: %s" % desired)
-    # print("actual : %s" % actual)
-    # print()
-#
+    substring = "ab"
+    longstring = "eidboaoooo"
+    desired = False
+    actual = checkInclusion(substring, longstring)
+    print()
+    print("sub, long : %s, %s" % (substring, longstring))
+    print("desired: %s" % desired)
+    print("actual : %s" % actual)
+    print()
+
+    substring = "ei"
+    longstring = "eidboaoooo"
+    desired = True
+    actual = checkInclusion(substring, longstring)
+    print()
+    print("sub, long : %s, %s" % (substring, longstring))
+    print("desired: %s" % desired)
+    print("actual : %s" % actual)
+    print()
+
+    substring = "oj"
+    longstring = "eidboaooooj"
+    desired = True
+    actual = checkInclusion(substring, longstring)
+    print()
+    print("sub, long : %s, %s" % (substring, longstring))
+    print("desired: %s" % desired)
+    print("actual : %s" % actual)
+    print()
+
+    substring = "aaa"
+    longstring = "eidboaaaaooooj"
+    desired = True
+    actual = checkInclusion(substring, longstring)
+    print()
+    print("sub, long : %s, %s" % (substring, longstring))
+    print("desired: %s" % desired)
+    print("actual : %s" % actual)
+    print()
+
 #
