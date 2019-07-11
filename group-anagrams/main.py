9c45b54bfb7b28769d5e9ea43add1b1b71f610e5
Stephen French <stephenfrench9@gmail.com>
2019-07-10 17:08:27 -0700

    group-anagrams: 15 mins to get a solution accepted. Runs in the 8 percentile for time complexity. Memory usage is 80 percentile. Not sure how I would make this faster.

---
+++ group-anagrams/main.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+class Solution(object):
+
+    def groupAnagrams(self, strs):
+        anagrams = {}
+
+        for w in strs:
+            key_gen = Counter(w)
+            key = ""
+            for l,c in sorted(key_gen.items()):
+               key = key + l + str(c)
+
+            try:
+                anagrams[key].append(w)
+            except:
+                anagrams[key] = [w]
+
+
+        ans = []
+        for k, group in anagrams.items():
+            ans.append(group)
+
+        return ans
+
+if __name__ == '__main__':
+    s = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    actual = Solution.groupAnagrams(Solution, s)
+    print()
+    print("argument : %s" % s)
+    print("actual   : " + str(actual))
+    print()
