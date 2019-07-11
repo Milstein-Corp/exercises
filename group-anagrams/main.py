24f918c8e1d6fb975fab0bb827dbf9b6d747e416
Stephen French <stephenfrench9@gmail.com>
2019-07-10 17:32:19 -0700

    group-anagrams: much faster this time around. I generate my anagram identifier by sorting the word

--- group-anagrams/main.py
+++ group-anagrams/main.py
@@ -1,27 +1,41 @@
 from collections import Counter
 
-class Solution(object):
 
+class Solution(object):
     def groupAnagrams(self, strs):
+        anagrams = {}
+        for w in strs:
+            key = "".join(sorted(w))
+            try:
+                anagrams[key].append(w)
+            except:
+                anagrams[key] = [w]
+
+        ans = []
+        for k, group in anagrams.items():
+            ans.append(group)
+        return ans
+
+    def groupAnagrams_first_solution(self, strs):
         anagrams = {}
 
         for w in strs:
             key_gen = Counter(w)
             key = ""
-            for l,c in sorted(key_gen.items()):
-               key = key + l + str(c)
+            for l, c in sorted(key_gen.items()):
+                key = key + l + str(c)
 
             try:
                 anagrams[key].append(w)
             except:
                 anagrams[key] = [w]
 
-
         ans = []
         for k, group in anagrams.items():
             ans.append(group)
 
         return ans
+
 
 if __name__ == '__main__':
     s = ["eat", "tea", "tan", "ate", "nat", "bat"]
